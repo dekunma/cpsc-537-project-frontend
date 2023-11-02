391ae4f43d40b22c5b09fae9f878eccc900123e7
Dekun Ma <william@dekun.me>
2023-11-01 22:27:21 -0400

    Add loading placeholder to people card

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar";
 
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
+
 import { useRequest } from "@src/service/useRequest";
 import { getAllExampleMbtiUsingGet } from "@src/service/apis/mbtiapp";
 import "react-loading-skeleton/dist/skeleton.css";
 import PersonCard from "../components/PersonCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
 
 export default function Index() {
   const fakePeopleData = [
@@ -33,19 +42,35 @@ export default function Index() {
     }
   );
 
-  useEffect(() => {
-    fetchExampleData();
+  const fetchFakeData = () => {
+    setPeopleData(emptyPeopleData);
     setTimeout(() => {
       setPeopleData(fakePeopleData);
     }, 3000);
+  };
+
+  useEffect(() => {
+    fetchExampleData();
+    fetchFakeData();
   }, []);
 
   return (
     <div>
       <SearchBar />
-      {peopleData.map((person, idx) => (
-        <PersonCard key={idx} title={person.name} subTitle={person.subTitle} />
-      ))}
+      <div className="w-full grid grid-cols-1">
+        <button className="btn btn-link w-full" onClick={fetchFakeData}>
+          <FontAwesomeIcon icon={faArrowsRotate} size="lg" />
+          10 New Random People
+        </button>
+
+        {peopleData.map((person, idx) => (
+          <PersonCard
+            key={idx}
+            title={person.name}
+            subTitle={person.subTitle}
+          />
+        ))}
+      </div>
     </div>
   );
 }
