3ac0f62b4af1d8139f65ea5c39a4e31c2f2a5eae
Dekun Ma <william@dekun.me>
2023-11-28 10:42:10 -0500

    Improved search bar dropdown UI

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,29 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { useRequest } from "@src/service/useRequest";
 import { getPeopleByPartialName } from "@src/service/apis/mbtiapp";
-import { useThrottle } from '../src/service/useThrottle';
+import { useThrottle } from "../src/service/useThrottle";
 
 export default function SearchBar({ onSelectName }) {
   const [isFocused, setIsFocused] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const throttledQuery = useThrottle(searchQuery, 700);
   const [getPeople, peopleSuggestions] = useRequest(getPeopleByPartialName);
 
   const handleSelectName = (name) => {
     setSearchQuery(name);
     onSelectName(name);
-  }
+  };
 
   const clearSearch = () => {
-    setSearchQuery('');
+    setSearchQuery("");
     onSelectName(null);
   };
 
   useEffect(() => {
     if (throttledQuery) {
-      getPeople({ query: throttledQuery })
+      getPeople({ query: throttledQuery });
     }
   }, [throttledQuery, getPeople]);
 
@@ -33,7 +33,9 @@ export default function SearchBar({ onSelectName }) {
         <input
           type="text"
           placeholder="Search a STAR"
-          className={`input w-full pr-10 ${isFocused ? "input-bordered input-primary" : "input-ghost"}`}
+          className={`input w-full pr-10 ${
+            isFocused ? "input-bordered input-primary" : "input-ghost"
+          }`}
           onFocus={() => setIsFocused(true)}
           onBlur={() => setIsFocused(false)}
           onChange={(e) => setSearchQuery(e.target.value)}
@@ -52,26 +54,23 @@ export default function SearchBar({ onSelectName }) {
         {searchQuery && peopleSuggestions && (
           <ul
             style={{
-              position: 'absolute',
-              width: '100%',
-              backgroundColor: '#333',
-              border: '1px solid #ddd',
-              boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
-              maxHeight: '200px',
-              overflowY: 'auto',
-              zIndex: 1000
+              position: "absolute",
+              width: "100%",
+              maxHeight: "200px",
+              overflowY: "auto",
+              zIndex: 1000,
             }}
+            className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box flex flex-row"
           >
             {peopleSuggestions.data.data.map((suggestion, index) => {
               // console.log("Suggestion at index", index, ":", suggestion.name);
               return (
-                <li key={index} onClick={() => handleSelectName(suggestion.name)} style={{
-                  padding: '8px 12px',
-                  cursor: 'pointer',
-                  borderBottom: '1px solid #eee',
-                  color: 'white',
-                }}>
-                  {suggestion.name}
+                <li
+                  key={index}
+                  onClick={() => handleSelectName(suggestion.name)}
+                  className="w-full"
+                >
+                  <a>{suggestion.name}</a>
                 </li>
               );
             })}
